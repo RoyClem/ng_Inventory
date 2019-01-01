10ca37129580e2f72030f2f8c0d36e193f4baba5
RoyClem <roy.clemmons@trec.texas.gov>
2019-01-01 07:10:37 -0600

    Initial commit

---
+++ src/app/product-image/product-image-component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, HostBinding } from '@angular/core';
+import { Product } from 'src/models/product.model'
+
+@Component({
+  selector: 'product-image',
+  templateUrl: './product-image-component.html',
+  styleUrls: ['./product-image-component.css']
+})
+export class ProductImageComponent {
+  @Input() product: Product;
+  @HostBinding('attr.class') cssClass = 'ui small image';  
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
